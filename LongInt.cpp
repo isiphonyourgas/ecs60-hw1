f5f22faa937d0544b56e39079c231d1746d42580
Jason Wong <jcwwong@ucdavis.edu>
2011-04-06 10:12:41 -0700

    testing on csif

--- LongInt.cpp
+++ LongInt.cpp
@@ -3,22 +3,37 @@
 #include <iostream>
 #include <iomanip>
 #include "LongInt.h"
+#include <string>
+#include <cstdlib>
 
 LongInt :: LongInt () 
 {
   content = 0;
+  next = NULL;
 }
 
 istream &operator >> (istream &input, LongInt &longint)
 {
-  input >> setw(9) >> longint.content;
-  //input >> longint.next();
+  string temp = "";
+  char c;
+  int i;
+  input.readsome(temp, 8);
+  cout << temp;
+  if (input.peek() != '\n')
+  {
+    longint.next = new LongInt;
+    input >> *(longint.next);
+  }
   return input;
 }
 
 ostream &operator << (ostream &output, const LongInt &longint)
 {
-  output << longint.content;
+  if (longint.next != NULL)
+  {
+   // output << longint.content;
+   // output << *(longint.next);
+  } else { cout << "NULL";}
   return output;
 }
 LongInt :: ~LongInt() {}
--- LongInt.h
+++ LongInt.h
@@ -14,4 +14,5 @@ public:
 //  int content;
 private:
   unsigned int content;
+  LongInt *next;
 };
